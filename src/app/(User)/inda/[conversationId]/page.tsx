1362d4ce799033dda7d0115fa124179da2d0028a
Rio Siahaan <222112324@stis.ac.id>
2025-07-11 13:15:27 +0700

    Modify all @ to ../ in conversationId page

--- src/app/(User)/inda/[conversationId]/page.tsx
+++ src/app/(User)/inda/[conversationId]/page.tsx
@@ -7,7 +7,7 @@ import SidebarInda from "../../../../component/User/Inda/Sidebar";
 import NavbarInda from "../../../../component/User/Inda/Navbar";
 // import ChatInda from "@/component/User/Inda/Chat";
 import ChatInda from "../../../../component/User/Inda/Chat";
-import { useDarkMode } from "@/lib/context/DarkModeContext";
+import { useDarkMode } from "../../../../lib/context/DarkModeContext";
 
 export default function IndaChat() {
   const [sidebar, setSidebar] = useState(true);

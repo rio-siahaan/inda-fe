266d40bf2698278b1c81dac34d142203905a8cf5
Rio Siahaan <222112324@stis.ac.id>
2025-07-15 11:58:37 +0700

    Change start or get conversation method

--- src/app/api/chat/startOrGetConversation/route.ts
+++ src/app/api/chat/startOrGetConversation/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../../../lib/prisma";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   const userId = body.id;
 

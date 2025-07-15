a6eb4e4314840c86111b1648bdb07449a00b8532
Rio Siahaan <222112324@stis.ac.id>
2025-07-15 09:04:29 +0700

    Remove prisma adapter from auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../lib/prisma";
 import bcrypt from "bcrypt";
 import { CustomPrismaAdapter } from "./prisma-adapter";

591d0c81c0c8a1984fc30c404e453bf585095583
Rio Siahaan <222112324@stis.ac.id>
2025-07-17 14:29:57 +0700

    Debugging chat ui

--- src/app/(User)/inda/[conversationId]/page.tsx
+++ src/app/(User)/inda/[conversationId]/page.tsx
@@ -7,11 +7,9 @@ import SidebarInda from "../../../../component/User/Inda/Sidebar";
 import NavbarInda from "../../../../component/User/Inda/Navbar";
 // import ChatInda from "@/component/User/Inda/Chat";
 import ChatInda from "../../../../component/User/Inda/Chat";
-import { useDarkMode } from "../../../../lib/context/DarkModeContext";
 
 export default function IndaChat() {
   const [sidebar, setSidebar] = useState(true);
-  const {dark} = useDarkMode()
 
   return (
     <div className={`flex`}>

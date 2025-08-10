0b96f6631793e6f48c1da0a744222062b83a1c97
rio-siahaan <riosiahaan323@gmail.com>
2025-08-10 10:28:47 +0700

    Debugging nextresponse list json

--- src/app/api/admin/listJson/route.ts
+++ src/app/api/admin/listJson/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   try {
     const listCsv = await ListFile()
 
-    if (listCsv.ok) return NextResponse.json(listCsv)
+    if (listCsv.ok) return listCsv
   } catch (error) {
     console.log(error)
     return NextResponse.json({status: 500})
